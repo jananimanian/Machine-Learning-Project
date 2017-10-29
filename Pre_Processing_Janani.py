84c65dd7cf450236691eea38560e6133a4f22f5b
Janani Subramaniyan <jananimanian@gmail.com>
2017-10-29 17:31:01 -0400

    updated

--- Pre_Processing_Janani.py
+++ Pre_Processing_Janani.py
@@ -88,3 +88,55 @@ print(train[v].describe())
 v = meta[(meta.level == 'binary') & (meta.keep)].index
 print("\n binary data");
 print(train[v].describe())
+
+desired_apriori=0.10
+
+# Get the indices per target value
+idx_0 = train[train.target == 0].index
+idx_1 = train[train.target == 1].index
+
+# Get original number of records per target value
+nb_0 = len(train.loc[idx_0])
+nb_1 = len(train.loc[idx_1])
+
+# Calculate the undersampling rate and resulting number of records with target=0
+undersampling_rate = ((1-desired_apriori)*nb_1)/(nb_0*desired_apriori)
+undersampled_nb_0 = int(undersampling_rate*nb_0)
+print('Rate to undersample records with target=0: {}'.format(undersampling_rate))
+print('Number of records with target=0 after undersampling: {}'.format(undersampled_nb_0))
+
+# Randomly select records with target=0 to get at the desired a priori
+undersampled_idx = shuffle(idx_0, random_state=37, n_samples=undersampled_nb_0)
+
+# Construct list with remaining indices
+idx_list = list(undersampled_idx) + list(idx_1)
+
+# Return undersample data frame
+train = train.loc[idx_list].reset_index(drop=True)
+
+vars_with_missing = []
+
+for f in train.columns:
+    missings = train[train[f] == -1][f].count()
+    if missings > 0:
+        vars_with_missing.append(f)
+        missings_perc = missings/train.shape[0]
+        
+        print('Variable {} has {} records ({:.2%}) with missing values'.format(f, missings, missings_perc))
+        
+print('In total, there are {} variables with missing values'.format(len(vars_with_missing)))
+
+
+
+# Dropping the variables with too many missing values
+vars_to_drop = ['ps_car_03_cat', 'ps_car_05_cat']
+train.drop(vars_to_drop, inplace=True, axis=1)
+meta.loc[(vars_to_drop),'keep'] = False  # Updating the meta
+
+# Imputing with the mean or mode
+mean_imp = Imputer(missing_values=-1, strategy='mean', axis=0)
+mode_imp = Imputer(missing_values=-1, strategy='most_frequent', axis=0)
+train['ps_reg_03'] = mean_imp.fit_transform(train[['ps_reg_03']]).ravel()
+train['ps_car_12'] = mean_imp.fit_transform(train[['ps_car_12']]).ravel()
+train['ps_car_14'] = mean_imp.fit_transform(train[['ps_car_14']]).ravel()
+train['ps_car_11'] = mode_imp.fit_transform(train[['ps_car_11']]).ravel()
